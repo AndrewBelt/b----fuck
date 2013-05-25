357b769cb25872c3d84f79fe9caf136be6c0db67
Andrew Belt <andrewpbelt@gmail.com>
2013-05-25 00:39:14 -0400

    Refixed the bracket stack

--- bfuck.c
+++ bfuck.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define BUFFER_SIZE 65536
+#define DATA_SIZE 65536
 #define BRACKET_STACK_SIZE 1024
 
 /*
@@ -26,10 +26,6 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	
-	/* Allocate the memory block */
-	
-	char *source = (char *) calloc(BUFFER_SIZE, sizeof(char));
-	
 	/* Read the entire file to memory */
 	
 	FILE *source_file = fopen(argv[1], "rb");
@@ -40,15 +36,19 @@ int main(int argc, char *argv[])
 		exit(1);
 	}
 	
-	size_t source_size = fread(source, sizeof(char), BUFFER_SIZE, source_file);
+	/* Allocate the memory block */
+	
+	char *source = (char *) calloc(DATA_SIZE, sizeof(char));
+	size_t source_size = fread(source, sizeof(char), DATA_SIZE, source_file);
 	fclose(source_file);
 
 	/* Initialize pointers */
 	
-	char *op = source;
 	char *data = source + source_size;
-	char *p = data;
 	char **bracket_stack = (char **) calloc(BRACKET_STACK_SIZE, sizeof(char *));
+
+	char *op = source;
+	char *p = data;
 	char **b = bracket_stack;
 	
 	/* Loop through each operation */
@@ -69,7 +69,9 @@ int main(int argc, char *argv[])
 				break;
 			case ',': *p = getchar();
 				break;
-			case ']': if (*p) op = *(b - 1);
+			case ']':
+				b--;
+				if (*p) op = *b - 1;
 				break;
 			case '[': *(b++) = op;
 				break;
@@ -77,6 +79,7 @@ int main(int argc, char *argv[])
 	} while (++op < data);
 
 	free(source);
+	free(bracket_stack);
 
 	return 0;
 }
